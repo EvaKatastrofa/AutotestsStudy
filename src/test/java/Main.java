380c2cbc87664481bff84f31b5fbe8abb1e5a8f9
Евгения Исмагилова <evgeniya.ism@gmail.com>
2023-11-22 18:01:14 +0300

    Правки, спринт 2

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         PostgreSQLTestContainer testContainer = new PostgreSQLTestContainer();
         testContainer.start();
+        System.out.println("stop");
     }
 }
--- src/test/java/containers/PostgreSQLTestContainer.java
+++ src/test/java/containers/PostgreSQLTestContainer.java
@@ -1,16 +1,8 @@
 package containers;
 
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.Network;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class PostgreSQLTestContainer extends GenericContainer<PostgreSQLTestContainer> {
     public static final String SERVICE_NAME = "POSTGRESQL_CONTAINER";
@@ -19,9 +11,7 @@ public class PostgreSQLTestContainer extends GenericContainer<PostgreSQLTestCont
     public PostgreSQLTestContainer() {
         super(DockerImageName.parse("postgres:13.3"));
 
-        this.withEnv("POSTGRES_USER", "test_user");
         this.withEnv("POSTGRES_PASSWORD", "qwerty");
-        this.withEnv("POSTGRES_DB", "test_db");
         this.withCreateContainerCmdModifier(cmd -> cmd.withName(SERVICE_NAME));
         this.withCopyFileToContainer(MountableFile.forClasspathResource("init_postgresql.sql"), "/docker-entrypoint-initdb.d/init_postgresql.sql");
     }

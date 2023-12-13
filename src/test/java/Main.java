c07d87bf26b7a635ab976c1aa442da7517bd110e
Евгения Исмагилова <evgeniya.ism@gmail.com>
2023-12-13 17:04:31 +0300

    Выполнение заданий, Спринт 3

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,9 +1,32 @@
+import clients.DBClient;
 import containers.PostgreSQLTestContainer;
+import models.db.test_schema.Tables;
+import models.db.test_schema.tables.records.StudentsRecord;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         PostgreSQLTestContainer testContainer = new PostgreSQLTestContainer();
         testContainer.start();
+
+        StudentsRecord record1 = new StudentsRecord(1, "Анна", "Экономика");
+        StudentsRecord record2 = new StudentsRecord(2, "Дмитрий", "Информатика");
+        StudentsRecord record3 = new StudentsRecord(3, "Анастасия", "Переводоведение");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        DBClient.openDBConnection("localhost", "test_db", "test_schema", "test_user", "qwerty");
+
+        DBClient.insertIntoTable(Tables.STUDENTS, record1);
+        DBClient.insertIntoTable(Tables.STUDENTS, record2);
+        DBClient.insertIntoTable(Tables.STUDENTS, record3);
+
+        DBClient.closeDBConnection();
+
         System.out.println("stop");
     }
 }
---
+++ src/test/java/clients/DBClient.java
@@ -0,0 +1,61 @@
+package clients;
+
+import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jooq.*;
+import org.jooq.Record;
+import org.jooq.conf.ParamType;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBClient {
+//    final String dbName = "test_db";
+//    final String user = "test_user";
+//    final String password = "qwerty";
+
+    private static String url = null;
+    @Getter
+    private static Connection connection;
+    private static Logger logger = LogManager.getLogger(DBClient.class);
+
+    public static void openDBConnection(String host, String database, String schema, String user, String password) throws SQLException {
+        url = String.format("jdbc:postgresql://%s:5432/%s?currentSchema=%s", host, database, schema);
+        connection = DriverManager.getConnection(url, user, password);
+        logger.info("Подключение к PostgreSQL создано");
+
+    }
+
+    public static void closeDBConnection() {
+        try {
+            connection.close();
+        } catch (Exception exception) {
+            logger.info("При запуске тестов не выполнялось подключение к БД");
+            logger.info(exception);
+        }
+
+        logger.info("Соединение с PostgreSQL успешно закрыто");
+    }
+
+    public static Result<Record> insertIntoTable(Table table, Record record) {
+        DSLContext create = DSL.using(connection, SQLDialect.POSTGRES);
+
+        String sql = create
+                .insertInto(table)
+                .set(record)
+                .getSQL(ParamType.INLINED);
+
+        logger.info("Запрос: {}", sql);
+        Result<Record> result = create.fetch(sql);
+        logger.info("\n" + result);
+
+        return result;
+    }
+
+
+
+
+}
